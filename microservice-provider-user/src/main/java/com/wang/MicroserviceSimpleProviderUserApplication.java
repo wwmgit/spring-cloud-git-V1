da8424fc4365a145534a476c9fdb9f3a25ad1bc3
wwmgit <2584118953@qq.com>
2018-02-04 11:27:18 +0800

    将user注册到eureka上

--- microservice-provider-user/src/main/java/com/wang/MicroserviceSimpleProviderUserApplication.java
+++ microservice-provider-user/src/main/java/com/wang/MicroserviceSimpleProviderUserApplication.java
@@ -2,8 +2,11 @@ package com.wang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient  //只支持Eureka
+//@EnableDiscoveryClient  可以作为其他的服务发现组件zk or Consul
 public class MicroserviceSimpleProviderUserApplication {
 
 	public static void main(String[] args) {
