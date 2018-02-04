d1916cbe65f85900f1ffc4f10cae3a06f3768ab4
wwmgit <2584118953@qq.com>
2018-02-04 13:42:16 +0800

    将movie注册到eureka上

--- microservice-consumer-movie/src/main/java/com/wang/MicroserviceConsumerMovieApplication.java
+++ microservice-consumer-movie/src/main/java/com/wang/MicroserviceConsumerMovieApplication.java
@@ -5,11 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient  //只支持Eureka
-//@EnableDiscoveryClient  可以作为其他的服务发现组件zk or Consul
-public class MicroserviceSimpleProviderUserApplication {
+@EnableEurekaClient
+public class MicroserviceConsumerMovieApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceSimpleProviderUserApplication.class, args);
+		SpringApplication.run(MicroserviceConsumerMovieApplication.class, args);
 	}
 }
--- microservice-consumer-movie/src/main/java/com/wang/controller/MovieController.java
+++ microservice-consumer-movie/src/main/java/com/wang/controller/MovieController.java
@@ -1,12 +1,18 @@
 package com.wang.controller;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import com.wang.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2018/2/2 0002.
@@ -19,6 +25,27 @@ public class MovieController {
 
     @Value("${user.userPath}")
     private String userPath;
+
+    @Autowired
+    private EurekaClient eurekaClient;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/eureka-instance-movie")
+    public String serviceUrl() {
+        InstanceInfo instance = eurekaClient.getNextServerFromEureka("microservice-consumer-movie", false);
+        return instance.getHomePageUrl();
+    }
+
+    @GetMapping("/eureka-instance-movie-info")
+    public List<ServiceInstance> getServiceInstance(){
+//        ServiceInstance serviceInstance = discoveryClient.getLocalServiceInstance();
+        List<ServiceInstance> serviceInstanceList = discoveryClient.getInstances("microservice-consumer-movie");
+        return serviceInstanceList;
+    }
+
+
 
     @GetMapping("/movie/{id}")
     public User findById(@PathVariable("id") Long id){
